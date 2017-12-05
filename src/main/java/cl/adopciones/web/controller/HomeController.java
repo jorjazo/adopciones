cb1e2527c847917870913e5b07a4cdee8220264e
Jorge Valencia <jorge@rebelsouls.io>
2017-12-05 14:38:16 -0300

    agregando handler para favicon

--- src/main/java/cl/adopciones/web/controller/HomeController.java
+++ src/main/java/cl/adopciones/web/controller/HomeController.java
@@ -1,9 +1,12 @@
 package cl.adopciones.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class HomeController {
@@ -26,6 +29,14 @@ public class HomeController {
     @GetMapping("/rescatistas")
     public String organizations() {
     	return "organizations";
+    }
+    
+    @GetMapping("/favicon.ico")
+    public ResponseEntity<?> getFavIcon(UriComponentsBuilder uriComponentsBuilder) {
+    	return ResponseEntity
+    		.status(HttpStatus.MOVED_PERMANENTLY)
+    		.location(uriComponentsBuilder.path("/static/img/favicon.png").build().toUri())
+    		.build();
     }
     
 }
