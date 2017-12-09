e38f5522c41570f855a44240ba755bebcacd7d42
Jorge Valencia <jorge@rebelsouls.io>
2017-12-09 20:47:37 -0300

    mas pruebas

---
+++ src/test/java/cl/adopciones/pets/PetTest.java
@@ -0,0 +1,29 @@
+package cl.adopciones.pets;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import cl.adopciones.organizations.Organization;
+import cl.adopciones.users.User;
+
+public class PetTest {
+
+	@Test
+	public void testIsInOwnersOrganization() {
+		Organization o = new Organization();
+		o.setId(0L);
+		User user = new User();
+		user.setId(1L);
+		user.setOrganization(o);
+		
+		Pet pet = new Pet();
+		pet.setOwner(user);
+		assertTrue(pet.isInOrganization(o));
+		
+		o = new Organization();
+		o.setId(0L);
+		assertTrue(pet.isInOrganization(o));
+	}
+	
+}
