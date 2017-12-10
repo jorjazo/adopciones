af50b7222d2dc00e229588af2a6d93bf5f526ab0
Jorge Valencia <jorge@rebelsouls.io>
2017-12-10 14:19:19 -0300

    configuracion para pruebas de integracion

--- src/main/java/cl/adopciones/config/DevConfig.java
+++ src/main/java/cl/adopciones/config/DevConfig.java
@@ -1,12 +1,24 @@
 package cl.adopciones.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.thymeleaf.TemplateEngine;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
 
 
+	@Bean
+	public TemplateEngine getTemplateEngine() {
+		return new TemplateEngine();
+	}
 	
+	@Bean
+	public JavaMailSender getMailSender() {
+		return new JavaMailSenderImpl();
+	}
 }
