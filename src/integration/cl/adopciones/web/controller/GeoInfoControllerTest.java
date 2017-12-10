f323157ca9325c9e17e82f28fa81e91469c1c53e
Jorge Valencia <jorge@rebelsouls.io>
2017-12-10 13:57:30 -0300

    agregando pruebas de integración

---
+++ src/integration/cl/adopciones/web/controller/GeoInfoControllerTest.java
@@ -0,0 +1,9 @@
+package cl.adopciones.web.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class GeoInfoControllerTest {
+
+}
