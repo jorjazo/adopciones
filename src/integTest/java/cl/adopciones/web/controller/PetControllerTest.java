25136220f24b5a0d9cee06461c197aab66e47b90
Jorge Valencia <jorge@rebelsouls.io>
2017-12-13 16:14:45 -0300

    refactoring de pruebas

--- src/integTest/java/cl/adopciones/web/controller/PetControllerTest.java
+++ src/integTest/java/cl/adopciones/web/controller/PetControllerTest.java
@@ -1,12 +1,13 @@
 package cl.adopciones.web.controller;
 
+import static io.rebelsouls.test.CompositeResultMatcher.createdAndRedirectResponse;
+import static io.rebelsouls.test.CompositeResultMatcher.htmlOkResponse;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
@@ -15,11 +16,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
 import cl.adopciones.pets.Gender;
 import cl.adopciones.pets.PetAgeCategory;
@@ -48,9 +50,7 @@ public class PetControllerTest {
 	@Test
 	public void shouldDisplayExistingPet() throws Exception {
 		mockMvc.perform(get(PetsController.URL_PREFIX + "/1"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-			.andExpect(content().encoding("UTF-8"))
+			.andExpect(htmlOkResponse())
 			.andExpect(content().string(containsString("Panzer")))
 			;
 	}
@@ -61,44 +61,47 @@ public class PetControllerTest {
 		String petName = "Misifús";
 		User user = userService.loadUserById(2L);
 		
-		mockMvc.perform(post(PetsController.URL_PREFIX).with(csrf()).with(user(user))
+		mockMvc.perform(post(PetsController.URL_PREFIX).with(csrf())
+				.with(user(user))
+				.with(newTestPetParams())
 				.param("name", petName)
-				.param("ageCategory", PetAgeCategory.OLDER_THAN_SIX_YEARS.name())
-				.param("comuna", Comuna.Algarrobo.name())
-				.param("description", "Un lindo gatito")
-				.param("gender", Gender.FEMALE.name())
-				.param("sizeCategory", PetSizeCategory.L.name())
-				.param("type", PetType.CAT.name())
 				)
-			.andExpect(status().isCreated())
-			.andExpect(redirectedUrlPattern("/mascotas/??"))
+			.andExpect(createdAndRedirectResponse("/mascotas/??"));
 		;
+	}
+	
+	private RequestPostProcessor newTestPetParams() {
+		return new RequestPostProcessor() {
+			@Override
+			public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+				request.addParameter("ageCategory", PetAgeCategory.OLDER_THAN_SIX_YEARS.name());
+				request.addParameter("comuna", Comuna.Algarrobo.name());
+				request.addParameter("description", "Un lindo gatito");
+				request.addParameter("gender", Gender.FEMALE.name());
+				request.addParameter("sizeCategory", PetSizeCategory.L.name());
+				request.addParameter("type", PetType.CAT.name());
+				return request;
+			}
+		};
 	}
 	
 	@Test
 	public void shouldNotCreateAPetIfMissingData() throws Exception {
-		String petName = "Misifús";
 		User user = userService.loadUserById(2L);
 		
-		mockMvc.perform(post(PetsController.URL_PREFIX).with(csrf()).with(user(user))
-				.param("name", petName)
-//				.param("ageCategory", PetAgeCategory.OLDER_THAN_SIX_YEARS.name())
-				.param("comuna", Comuna.Algarrobo.name())
-				.param("description", "Un lindo gatito")
-				.param("gender", Gender.FEMALE.name())
-				.param("sizeCategory", PetSizeCategory.L.name())
-				.param("type", PetType.CAT.name())
+		mockMvc.perform(post(PetsController.URL_PREFIX)
+				.with(csrf())
+				.with(user(user))
+				.with(newTestPetParams())
 				)
-			.andExpect(status().isBadRequest())
+			.andExpect(status().isBadRequest()) //missing name
 		;
 	}
 	
 	@Test
 	public void shouldDisplayAFormToCreateANewPet() throws Exception {
 		mockMvc.perform(get(PetsController.URL_PREFIX + "/nuevo"))
-		.andExpect(status().isOk())
-		.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-		.andExpect(content().encoding("UTF-8"))
+		.andExpect(htmlOkResponse())
 		.andExpect(content().string(containsString("MALE")))
 		;
 	}
---
+++ src/integTest/java/io/rebelsouls/test/CompositeResultMatcher.java
@@ -0,0 +1,43 @@
+package io.rebelsouls.test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import lombok.Builder;
+import lombok.Singular;
+
+@Builder
+public class CompositeResultMatcher implements ResultMatcher {
+
+	@Singular
+	private List<ResultMatcher> matchers;
+	
+	@Override
+	public void match(MvcResult result) throws Exception {
+		if(matchers == null) return;
+		for(ResultMatcher rm: matchers)
+			rm.match(result);
+	}
+
+	public static CompositeResultMatcher htmlOkResponse() {
+		return CompositeResultMatcher.builder()
+				.matcher(status().isOk())
+				.matcher(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+				.matcher(content().encoding("UTF-8"))
+				.build();
+	}
+	
+	public static CompositeResultMatcher createdAndRedirectResponse(final String urlAntPattern) {
+		return CompositeResultMatcher.builder()
+				.matcher(status().isCreated())
+				.matcher(redirectedUrlPattern(urlAntPattern))
+				.build();
+	}
+}
