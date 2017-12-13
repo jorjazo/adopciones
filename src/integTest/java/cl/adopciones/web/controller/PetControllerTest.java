0a7d81a5d1d95e03b57553fdf2de471310a4212c
Jorge Valencia <jorge@rebelsouls.io>
2017-12-13 17:15:21 -0300

    prueba para fotos

--- src/integTest/java/cl/adopciones/web/controller/PetControllerTest.java
+++ src/integTest/java/cl/adopciones/web/controller/PetControllerTest.java
@@ -3,6 +3,8 @@ package cl.adopciones.web.controller;
 import static io.rebelsouls.test.CompositeResultMatcher.createdAndRedirectResponse;
 import static io.rebelsouls.test.CompositeResultMatcher.htmlOkResponse;
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,12 +12,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -31,6 +39,8 @@ import cl.adopciones.users.User;
 import cl.adopciones.users.UserService;
 import io.rebelsouls.chile.Comuna;
 import io.rebelsouls.services.StorageService;
+import io.rebelsouls.storage.StorageResource;
+import io.rebelsouls.storage.StorageResourceDescription;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -113,6 +123,35 @@ public class PetControllerTest {
 		mockMvc.perform(get(PetsController.URL_PREFIX + "/nuevo"))
 		.andExpect(htmlOkResponse())
 		.andExpect(content().string(containsString("MALE")))
+		;
+	}
+	
+	@Value("classpath:/static/img/logo-edra.jpg") Resource testPhoto;
+	
+	@Test
+	public void shouldDisplayPetPhoto() throws Exception {
+		String photoPath = "1/photos/0/original";
+
+		StorageResourceDescription description = new StorageResourceDescription();
+		description.setContentLength(testPhoto.contentLength());
+		description.setLastModified(new Date(testPhoto.lastModified()));
+		description.setPath(photoPath);
+		
+		when(storageService.list(anyString()))
+			.thenReturn(Arrays.asList(description));
+		
+		StorageResource resource = new StorageResource();
+		resource.setContentLength(testPhoto.contentLength());
+		resource.setPath(photoPath);
+		resource.setLastModified(new Date(testPhoto.lastModified()));
+		resource.setContentStream(testPhoto.getInputStream());
+		resource.setContentType(MediaType.IMAGE_JPEG_VALUE);
+		
+		when(storageService.load(photoPath)).thenReturn(resource);
+		
+		mockMvc.perform(get(PetsController.URL_PREFIX + "/1/fotos/0/original"))
+		.andExpect(status().isOk())
+		.andExpect(content().contentTypeCompatibleWith(MediaType.IMAGE_JPEG))
 		;
 	}
 }
