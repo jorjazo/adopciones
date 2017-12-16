4d516c65bd9b590cb57e930965c10600928de7c3
Jorge Valencia <jorge@rebelsouls.io>
2017-12-16 14:38:33 -0300

    cambiando a lambda

--- src/main/java/cl/adopciones/pets/PetSpecifications.java
+++ src/main/java/cl/adopciones/pets/PetSpecifications.java
@@ -1,10 +1,5 @@
 package cl.adopciones.pets;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 import org.springframework.data.jpa.domain.Specification;
 
 import io.rebelsouls.chile.Comuna;
@@ -13,86 +8,66 @@ import io.rebelsouls.chile.Region;
 
 public class PetSpecifications {
 
+	private PetSpecifications() {
+		
+	}
+	
 	public static Specification<Pet> hasName(String name) {
-		return new Specification<Pet>() {
-			@Override
-			public Predicate toPredicate(Root<Pet> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				if(name == null)
-					return cb.and();
-				return cb.equal(root.get("name"), name);
-			}
-			
+		return (root, query, cb) -> {
+			if(name == null)
+				return cb.and();
+			return cb.equal(root.get("name"), name);
 		};
 	}
 	
 	public static Specification<Pet> genderIn(Gender[] genders) {
-		return new Specification<Pet>() {
-			@Override
-			public Predicate toPredicate(Root<Pet> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				if(genders == null)
-					return cb.or();
-				
-				return root.get("gender").in((Object[]) genders);
-			}
+		return (root, query, cb) -> {
+			if(genders == null)
+				return cb.or();
 			
+			return root.get("gender").in((Object[]) genders);
 		};
 	}
 	
 	public static Specification<Pet> typeIn(PetType[] types) {
-		return new Specification<Pet>() {
-			@Override
-			public Predicate toPredicate(Root<Pet> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				if(types == null)
-					return cb.or();
-				
-				return root.get("type").in((Object[]) types);
-			}
+		return (root, query, cb) -> {
+			if(types == null)
+				return cb.or();
 			
+			return root.get("type").in((Object[]) types);
 		};
 	}
 	
 	public static Specification<Pet> sizeIn(PetSizeCategory[] sizes) {
-		return new Specification<Pet>() {
-			@Override
-			public Predicate toPredicate(Root<Pet> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				if(sizes == null)
-					return cb.or();
-				
-				return root.get("sizeCategory").in((Object[]) sizes);
-			}
+		return (root, query, cb) -> {
+			if(sizes == null)
+				return cb.or();
 			
+			return root.get("sizeCategory").in((Object[]) sizes);
 		};
 	}
 	
 	public static Specification<Pet> ageIn(PetAgeCategory[] ages) {
-		return new Specification<Pet>() {
-			@Override
-			public Predicate toPredicate(Root<Pet> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				if(ages == null)
-					return cb.or();
-				
-				return root.get("ageCategory").in((Object[]) ages);
-			}
+		return (root, query, cb) -> {
+			if(ages == null)
+				return cb.or();
 			
+			return root.get("ageCategory").in((Object[]) ages);
 		};
 	}
 	
 	public static Specification<Pet> inLocation(Region region, Provincia provincia, Comuna comuna) {
-		return new Specification<Pet>() {
-			@Override
-			public Predicate toPredicate(Root<Pet> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-				if(comuna != null)
-					return cb.equal(root.get("location"), comuna);
-				
-				if(provincia != null)
-					return root.get("location").in(provincia.getComunas().toArray());
-				
-				if(region != null)
-					return root.get("location").in(region.getComunas().toArray());
-				
-				return cb.and();
-			}
+		return (root, query, cb) -> {
+			if(comuna != null)
+				return cb.equal(root.get("location"), comuna);
 			
+			if(provincia != null)
+				return root.get("location").in(provincia.getComunas().toArray());
+			
+			if(region != null)
+				return root.get("location").in(region.getComunas().toArray());
+			
+			return cb.and();
 		};
 	}
 	
